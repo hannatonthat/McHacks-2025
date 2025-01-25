0eeb67cd73c544f8f1c05b56ee19c27c074f7315
Hanna Ton That <hannatonthat@gmail.com>
2025-01-25 15:00:50 -0500

    overall styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,16 @@ import '../Home.css'
 
 export default function Home() {
     return (
-        <p></p>
+        <>
+        <div class='background'>
+            <nav>
+                <div class='logo'><a href='/'>Match-A-Wish</a></div>
+                <div class='links'>
+                    <li><a href='Login' onclick='toggleMenu()'>Login</a></li>
+                    <li><a href='Register' onclick='toggleMenu()'>Register</a></li>
+                </div>
+            </nav>
+        </div>
+        </>
     )
 }
