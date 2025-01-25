0e53975e7183d909d63715a09f5f4312fb516f48
Hanna Ton That <hannatonthat@gmail.com>
2025-01-25 16:03:31 -0500

    home page skeleton

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,16 +2,35 @@ import React from 'react'
 import '../Home.css'
 
 export default function Home() {
+
+    const handleLoginClick = () => {
+        window.location.href = '/login';
+    };
+
+    const handleRegisterClick = () => {
+        window.location.href = '/register';
+    };
+
     return (
         <>
-        <div class='background'>
+        <div className='background'>
             <nav>
-                <div class='logo'><a href='/'>Match-A-Wish</a></div>
-                <div class='links'>
-                    <li><a href='Login' onclick='toggleMenu()'>Login</a></li>
-                    <li><a href='Register' onclick='toggleMenu()'>Register</a></li>
+                <div className='logo'>
+                    <a href='/'>Match-A-Wish</a>
+                </div>
+                <div className='links'>
+                    <li><a href='login'>Login</a></li>
+                    <li><a href='register'>Register</a></li>
                 </div>
             </nav>
+            <div className='section'>
+                <h1>Match-A-Wish</h1>
+                <h2>Turning anxious waits into joyful memories, one wish at a time.</h2>
+                <div className='buttons'>
+                    <li><button onClick={handleLoginClick}>Login</button></li>
+                    <li><button onClick={handleRegisterClick}>Register</button></li>
+                </div>
+            </div>
         </div>
         </>
     )
