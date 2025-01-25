0a4bba177ea19df88e6f684e9b09f601fa6753f9
Hanna Ton That <hannatonthat@gmail.com>
2025-01-25 12:13:44 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
