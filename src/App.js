781fc8ab24e9dd933332fab6606f832f8f04d3ab
Hanna Ton That <hannatonthat@gmail.com>
2025-01-25 14:23:01 -0500

    frontend template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Admin from './Pages/Admin';
+import Donor from './Pages/Donor';
+import Hospital from './Pages/Hospital';
 import './App.css';
 
 function App() {
   return (
-    <p></p>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/donor" element={<Donor />} />
+          <Route path="/hospital" element={<Hospital />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
